05394dcf6338e00d3c134ac0b1fee7b53abc0114
Nathan Walker <walkerrunpdx@gmail.com>
2020-11-19 14:11:13 -0800

    chore: cleanup

--- packages/angular/dist/esm2015/index.js
+++
@@ -1,2 +0,0 @@
-export * from './lib/angular';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiLi4vLi4vLi4vc3JjLyIsInNvdXJjZXMiOlsiaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsY0FBYyxlQUFlLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tICcuL2xpYi9hbmd1bGFyJztcbiJdfQ==
