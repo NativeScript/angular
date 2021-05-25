c22d5a007003e232fc45fb3be18942f2a95f50f3
Eduardo Speroni <edusperoni@gmail.com>
2021-05-25 18:06:45 -0300

    fix ng-packagr warnings 2

--- packages/angular/src/lib/legacy/router/index.ts
+++ packages/angular/src/lib/legacy/router/index.ts
@@ -1,3 +1,3 @@
 export { NSLocationStrategy } from './ns-location-strategy';
-export * from './ns-route-reuse-strategy';
+export { NSRouteReuseStrategy } from './ns-route-reuse-strategy';
 export * from './router.module';
