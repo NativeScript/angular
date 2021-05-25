dbf7d305e1c3d21e45802b3465138b7463462604
Eduardo Speroni <edusperoni@gmail.com>
2021-05-25 18:06:21 -0300

    fix ng-packag warnings

--- packages/angular/src/lib/animations/index.ts
+++ packages/angular/src/lib/animations/index.ts
@@ -1,4 +1,4 @@
-export * from './animations.module';
-export * from './animation-player';
-export * from './animation-driver';
-export * from './utils';
+export { InjectableAnimationEngine, NativeScriptAnimationsModule, instantiateDefaultStyleNormalizer, instantiateRendererFactory, instantiateSupportedAnimationDriver } from './animations.module';
+export { NativeScriptAnimationPlayer } from './animation-player';
+export { NativeScriptAnimationDriver } from './animation-driver';
+export { Keyframe, createKeyframeAnimation, dashCaseToCamelCase } from './utils';
