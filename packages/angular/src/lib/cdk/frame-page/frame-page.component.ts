899e2d544aaa5d4414ba8aaad6f68d338736a912
Eduardo Speroni <edusperoni@gmail.com>
2021-05-31 20:06:55 -0300

    allow framePage to remove actionbar

--- packages/angular/src/lib/cdk/frame-page/frame-page.component.ts
+++ packages/angular/src/lib/cdk/frame-page/frame-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { Frame } from '@nativescript/core';
 import { frameMeta, registerElement } from '../../element-registry';
+import { PageDirective } from './page.directive';
 
 export function customFrameComponentFactory(v: FramePageComponent) {
   return v.element.nativeElement;
@@ -10,7 +11,7 @@ registerElement('FramePage', () => Frame, frameMeta);
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'FramePage',
-  template: `<Page><ng-content></ng-content></Page>`,
+  template: `<Page #page><ng-content></ng-content></Page>`,
   providers: [
     {
       provide: Frame,
@@ -19,6 +20,23 @@ registerElement('FramePage', () => Frame, frameMeta);
     },
   ],
 })
-export class FramePageComponent {
+export class FramePageComponent implements AfterViewInit, OnChanges {
+  @ViewChild(PageDirective) page: PageDirective;
+
+  @Input() actionBarHidden = false;
+
   constructor(public element: ElementRef<Frame>) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.actionBarHidden) {
+      if (this.page?.element.nativeElement) {
+        this.page.element.nativeElement.actionBarHidden = !!this.actionBarHidden;
+      }
+    }
+  }
+  ngAfterViewInit(): void {
+    if (this.page?.element.nativeElement) {
+      this.page.element.nativeElement.actionBarHidden = !!this.actionBarHidden;
+    }
+  }
 }
