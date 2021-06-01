60493eaf9fe04ea73371823b17fd8195a6545907
Nathan Walker <walkerrunpdx@gmail.com>
2021-05-31 21:14:49 -0700

    chore: http example - not working still

--- apps/nativescript-demo-ng/src/app/item/items.component.ts
+++ apps/nativescript-demo-ng/src/app/item/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Item } from './item';
 import { ItemService } from './item.service';
@@ -14,7 +15,7 @@ export class ItemsComponent implements OnInit {
   message = 'Hello Angular 12!';
   items: Array<Item>;
 
-  constructor(private itemService: ItemService, private nativeDialog: NativeDialogService, private modalDialog: ModalDialogService) {}
+  constructor(private itemService: ItemService, private nativeDialog: NativeDialogService, private modalDialog: ModalDialogService, private http: HttpClient) {}
 
   ngOnInit(): void {
     console.log('ItemsComponent ngOnInit');
@@ -32,6 +33,12 @@ export class ItemsComponent implements OnInit {
     ref.beforeClosed().subscribe((result) => console.log('beforeClosed', result));
     ref.afterClosed().subscribe((result) => console.log('afterClosed', result));
     // setTimeout(() => ref.close('result!'), 1000);
+  }
+
+  fetchTodos() {
+    this.http.get(`https://jsonplaceholder.typicode.com/todos/1`).subscribe((res) => {
+      console.log('Http get:', res);
+    });
   }
 
   ngOnDestroy() {
