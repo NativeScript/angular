c1808455d01ed4be53244acab5ec03eb7a4bbf62
Eduardo Speroni <edusperoni@gmail.com>
2021-06-07 16:02:15 -0300

    patch view class again

--- packages/zone-js/dist/events.ts
+++ packages/zone-js/dist/events.ts
@@ -3,7 +3,7 @@ import './core';
 import { Observable, View } from '@nativescript/core';
 
 Zone.__load_patch('nativescript_observable_events', (g, z, api: any) => {
-  api.patchNativeScriptEventTarget(g, api, [Observable, Observable.prototype, View]);
+  api.patchNativeScriptEventTarget(g, api, [Observable, Observable.prototype, View, View.prototype]);
 });
 
 Zone.__load_patch('nativescript_xhr_events', (g, z, api: any) => {
