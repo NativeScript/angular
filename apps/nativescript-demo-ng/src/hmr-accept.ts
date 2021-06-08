44c223baeaa1df92f5c43519cd366a7c6aaf7932
Nathan Walker <walkerrunpdx@gmail.com>
2021-06-08 11:04:19 -0700

    chore: cleanup

--- apps/nativescript-demo-ng/src/hmr-accept.ts
+++
@@ -1,9 +0,0 @@
-export function hmrAccept(mod: any) {
-  if (!mod.hot) {
-    return;
-  }
-  mod.hot.accept();
-  mod.hot.dispose(() => {
-    if (global['__cleanup_ng_hot__']) global['__cleanup_ng_hot__']();
-  });
-}
--- apps/nativescript-demo-ng/src/main.ts
+++ apps/nativescript-demo-ng/src/main.ts
@@ -1,12 +1,6 @@
-import { platformNativeScript, platformNativeScriptDynamic, runNativeScriptAngularApp } from '@nativescript/angular';
-import { Application, StackLayout } from '@nativescript/core';
+import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
 
 import { AppModule } from './app/app.module';
-import { hmrAccept } from './hmr-accept';
-
-hmrAccept(module);
-
-// platformNativeScriptDynamic().bootstrapModule(AppModule);
 
 runNativeScriptAngularApp({
   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
