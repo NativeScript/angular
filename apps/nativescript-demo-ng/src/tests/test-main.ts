5e58fee5c2f9a3768b3b4f2d887d52b4928f0b97
Eduardo Speroni <edusperoni@gmail.com>
2021-06-08 15:15:09 -0300

    chore: cleanup

--- apps/nativescript-demo-ng/src/tests/test-main.ts
+++ apps/nativescript-demo-ng/src/tests/test-main.ts
@@ -6,7 +6,6 @@ import '@nativescript/zone-js';
 import 'zone.js/testing';
 import { TestBed } from '@angular/core/testing';
 import { NativeScriptTestingModule } from '@nativescript/angular/testing';
-import { NativeScriptModule } from '@nativescript/angular';
 import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 TestBed.initTestEnvironment([NativeScriptTestingModule], platformBrowserDynamicTesting());
