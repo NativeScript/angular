4e15564e1aa42246138ba984e9526b73a442a11b
Nathan Walker <walkerrunpdx@gmail.com>
2021-06-27 18:42:28 -0700

    chore: Nx 12.4

--- apps/nativescript-demo-ng/references.d.ts
+++ apps/nativescript-demo-ng/references.d.ts
@@ -1,5 +1,5 @@
-/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
-/// <reference path="./node_modules/@nativescript/types-android/lib/android-29.d.ts" />
+/// <reference path="../../node_modules/@nativescript/types-ios/index.d.ts" />
+/// <reference path="../../node_modules/@nativescript/types-android/lib/android-29.d.ts" />
 
 declare namespace NodeJS {
   interface Global {
