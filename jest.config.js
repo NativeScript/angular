c08d668d96f932f39950d366b8058a0773b4757b
Nathan Walker <walkerrunpdx@gmail.com>
2021-08-13 13:51:11 -0700

    chore: migrate Nx 12.7.1

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  projects: ['<rootDir>/packages/angular', '<rootDir>/packages/zone-js'],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
