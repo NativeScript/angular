576a474565dd5afd7ca66d27d765356dfb3cdb9e
Nathan Walker <walkerrunpdx@gmail.com>
2021-08-13 17:08:27 -0700

    chore: 12.1.0-rc.1

--- apps/nativescript-demo-ng/src/main.ts
+++ apps/nativescript-demo-ng/src/main.ts
@@ -1,7 +1,12 @@
-import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
+import { NativeScriptNgZone, platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
 
 import { AppModule } from './app/app.module';
 
 runNativeScriptAngularApp({
-  appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
+  appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule, { ngZone: new NativeScriptNgZone() }),
 });
+
+// Comment above and Uncomment below to try without custom NgZone:
+// runNativeScriptAngularApp({
+//   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
+// });
