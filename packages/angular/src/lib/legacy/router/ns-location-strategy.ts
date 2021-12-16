1ab746dadcfabe45b7c31144bcb781d394def392
Eduardo Speroni <edusperoni@gmail.com>
2021-12-16 18:35:38 -0300

    fix: pass correct LocationChangeEvent

--- packages/angular/src/lib/legacy/router/ns-location-strategy.ts
+++ packages/angular/src/lib/legacy/router/ns-location-strategy.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable, Optional } from '@angular/core';
-import { LocationStrategy } from '@angular/common';
+import { LocationChangeEvent, LocationStrategy } from '@angular/common';
 import { DefaultUrlSerializer, UrlSegmentGroup, UrlTree, ActivatedRouteSnapshot, Params } from '@angular/router';
 import { Frame } from '@nativescript/core';
 import { NativeScriptDebug } from '../../trace';
@@ -243,7 +243,7 @@ export class NSLocationStrategy extends LocationStrategy {
     }
 
     const url = urlSerializer.serialize(this.currentUrlTree);
-    const change = { url: url, pop: pop };
+    const change: LocationChangeEvent = { state, type: 'popstate' };
     for (let fn of this.popStateCallbacks) {
       fn(change);
     }
