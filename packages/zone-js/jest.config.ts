33c01b1b49a465b02ca0bc89785933b5b30e4a4e
Nathan Walker <walkerrunpdx@gmail.com>
2023-03-06 10:27:08 -0800

    chore: nx migrate latest

--- packages/zone-js/jest.config.ts
+++ packages/zone-js/jest.config.ts
@@ -2,12 +2,10 @@
 export default {
   displayName: 'zone-js',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/zone-js',
