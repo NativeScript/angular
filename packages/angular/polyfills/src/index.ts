b95db741957c9af2b2240939455d09abc235e490
Eduardo Speroni <edusperoni@gmail.com>
2023-11-08 14:51:10 -0300

    feat: add partial polyfills to performance

--- packages/angular/polyfills/src/index.ts
+++ packages/angular/polyfills/src/index.ts
@@ -1,18 +1,32 @@
-import { installPolyfills } from "@nativescript/core/globals";
-
 export const nsNgPolyfills = true;
 
-// Angular 17 uses performance.mark in @angular/core
-let loadTime = Date.now();
-export const globalPolyfills = {
-    performance: {
-        now() {
-            return Date.now() - loadTime;
+global.performance ??= {} as unknown as Performance;
+
+function getPerformanceObject() {
+  const loadTime = Date.now();
+  const performance: Partial<Performance> = {
+    timeOrigin: loadTime,
+    now() {
+      return Date.now() - loadTime;
+    },
+    mark(name: string, options: PerformanceMarkOptions) {
+      return {
+        detail: options?.detail,
+        duration: 0,
+        entryType: 'mark',
+        name,
+        startTime: options?.startTime ?? Date.now() - loadTime,
+        toJSON: () => {
+          ('');
         },
-        mark(name: string, options: any) {
-            // Note: we could add impl
-        }
-    }
+      };
+    },
+  };
+  return performance;
 }
-global.registerModule('performance', () => globalPolyfills);
-installPolyfills('performance', ['performance']);
+
+const polyfilledPerformance = getPerformanceObject();
+
+for (const key in polyfilledPerformance) {
+  global.performance[key] ??= polyfilledPerformance[key];
+}
