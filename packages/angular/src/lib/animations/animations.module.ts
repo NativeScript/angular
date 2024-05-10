ad576bfea2830637933bb1fe5d90bd97e2b187c8
darklight365 <darklight_365@yahoo.com>
2024-05-09 22:37:10 -0400

    fix: animations (#132)

--- packages/angular/src/lib/animations/animations.module.ts
+++ packages/angular/src/lib/animations/animations.module.ts
@@ -1,13 +1,11 @@
 import { NgModule, Injectable, Inject, NgZone, RendererFactory2, Optional, SkipSelf } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-import { AnimationBuilder } from '@angular/animations';
+import { AnimationBuilder, ɵBrowserAnimationBuilder as BrowserAnimationBuilder } from '@angular/animations';
 
-import { AnimationDriver, ɵAnimationStyleNormalizer as AnimationStyleNormalizer, ɵWebAnimationsStyleNormalizer as WebAnimationsStyleNormalizer, ɵAnimationEngine as AnimationEngine } from '@angular/animations/browser';
-
-// import { ɵAnimationRendererFactory as AnimationRendererFactory, ɵBrowserAnimationBuilder as BrowserAnimationBuilder } from '@angular/platform-browser/animations';
+import { AnimationDriver, ɵAnimationRendererFactory as AnimationRendererFactory, ɵAnimationStyleNormalizer as AnimationStyleNormalizer, ɵWebAnimationsStyleNormalizer as WebAnimationsStyleNormalizer, ɵAnimationEngine as AnimationEngine } from '@angular/animations/browser';
 
 // import { NativeScriptModule } from "../nativescript.module";
-// import { NativeScriptRendererFactory } from '../nativescript-renderer';
+import { NativeScriptRendererFactory } from '../nativescript-renderer';
 import { NativeScriptAnimationDriver } from './animation-driver';
 import { throwIfAlreadyLoaded } from '../utils/general';
 import { NativeScriptCommonModule } from '../nativescript-common.module';
@@ -15,7 +13,7 @@ import { NativeScriptCommonModule } from '../nativescript-common.module';
 @Injectable()
 export class InjectableAnimationEngine extends AnimationEngine {
   constructor(@Inject(DOCUMENT) doc: any, driver: AnimationDriver, normalizer: AnimationStyleNormalizer) {
-    super(doc.body, driver, normalizer);
+    super(doc, driver, normalizer);
   }
 }
 
@@ -23,9 +21,9 @@ export function instantiateSupportedAnimationDriver() {
   return new NativeScriptAnimationDriver();
 }
 
-// export function instantiateRendererFactory(renderer: NativeScriptRendererFactory, engine: AnimationEngine, zone: NgZone) {
-//   return new AnimationRendererFactory(renderer, engine, zone);
-// }
+export function instantiateRendererFactory(renderer: NativeScriptRendererFactory, engine: AnimationEngine, zone: NgZone) {
+  return new AnimationRendererFactory(renderer, engine, zone);
+}
 
 export function instantiateDefaultStyleNormalizer() {
   return new WebAnimationsStyleNormalizer();
@@ -38,17 +36,17 @@ export function instantiateDefaultStyleNormalizer() {
       provide: AnimationDriver,
       useFactory: instantiateSupportedAnimationDriver,
     },
-    // { provide: AnimationBuilder, useClass: BrowserAnimationBuilder },
+    { provide: AnimationBuilder, useClass: BrowserAnimationBuilder },
     {
       provide: AnimationStyleNormalizer,
       useFactory: instantiateDefaultStyleNormalizer,
     },
     { provide: AnimationEngine, useClass: InjectableAnimationEngine },
-    // {
-    //   provide: RendererFactory2,
-    //   useFactory: instantiateRendererFactory,
-    //   deps: [NativeScriptRendererFactory, AnimationEngine, NgZone],
-    // },
+    {
+      provide: RendererFactory2,
+      useFactory: instantiateRendererFactory,
+      deps: [NativeScriptRendererFactory, AnimationEngine, NgZone],
+    },
   ],
 })
 export class NativeScriptAnimationsModule {
