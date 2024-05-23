0c85c9345c5b552684f8f7edb8b3de444c02915c
Nathan Walker <walkerrunpdx@gmail.com>
2024-05-22 18:03:54 -0700

    chore: demo updates

--- apps/nativescript-demo-ng/src/app/item/items.component.ts
+++ apps/nativescript-demo-ng/src/app/item/items.component.ts
@@ -12,7 +12,7 @@ import { ModalDialogService, NativeDialogService } from '@nativescript/angular';
   templateUrl: './items.component.html',
 })
 export class ItemsComponent implements OnInit {
-  message = 'Hello Angular 17!';
+  message = 'Hello Angular 18!';
   items: Array<Item>;
 
   constructor(private itemService: ItemService, private nativeDialog: NativeDialogService, private modalDialog: ModalDialogService, private http: HttpClient) {}
